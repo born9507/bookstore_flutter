f331e6a31efcd7bd03df9d068fa1e33699fa319e
Sangwon <born9507@snu.ac.kr>
2022-03-04 04:47:47 +0900

    [fix] solution

--- lib/book_service.dart
+++ lib/book_service.dart
@@ -5,4 +5,14 @@ import 'book.dart';
 class BookService extends ChangeNotifier {
   // 책 목록
   List<Book> bookList = [];
+
+  void createBook(Book book) {
+    bookList.add(book);
+    notifyListeners();
+  }
+
+  void clearBooks() {
+    bookList = [];
+    notifyListeners();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:bookstore/book_service.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'book.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => BookService())],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,9 +29,91 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
+  TextEditingController _textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Consumer<BookService>(builder: (context, bookService, child) {
+      return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text(
+            'Book Store',
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.white,
+          bottom: PreferredSize(
+            preferredSize: Size(0, 60),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 8),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    child: Text(
+                      'total 0',
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                  TextField(
+                    controller: _textEditingController,
+                    decoration: InputDecoration(
+                      hintText: '원하시는 책을 검색해주세요.',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.search),
+                        onPressed: () async {
+                          if (_textEditingController.text.length > 0) {
+                            Response result = await Dio().get(
+                                'https://www.googleapis.com/books/v1/volumes',
+                                queryParameters: {
+                                  'q': _textEditingController.text,
+                                  'startIndex': 0,
+                                  'maxResults': 10,
+                                });
+
+                            bookService.clearBooks();
+
+                            for (var item in result.data['items']) {
+                              Book book = Book(
+                                title: item['volumeInfo']['title'] ?? '',
+                                subtitle: item['volumeInfo']['subtitle'] ?? '',
+                                thumbnail: item['volumeInfo']['imageLinks']
+                                        ['thumbnail'] ??
+                                    '',
+                                previewLink:
+                                    item['volumeInfo']['previewLink'] ?? '',
+                              );
+
+                              bookService.createBook(book);
+                            }
+                          }
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: bookService.bookList.length,
+          itemBuilder: ((context, index) {
+            Book book = bookService.bookList[index];
+            return ListTile(
+              leading: Image.network(book.thumbnail),
+              title: Text(book.title),
+              subtitle: Text(book.subtitle),
+              onTap: () {
+                launch(book.previewLink);
+              },
+            );
+          }),
+        ),
+      );
+    });
   }
 }
